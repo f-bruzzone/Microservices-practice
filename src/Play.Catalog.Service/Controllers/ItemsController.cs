72487ecd3e7022bbe4c076b6d2b97fb7f23fd193
f-bruzzone <fb88@njit.edu>
2023-01-27 18:54:06 -0500

    finished catalog service initial api with some checks in place

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -21,9 +21,12 @@ namespace Play.Catalog.Service.Controllers
         }
 
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item == null) return NotFound();
+
             return item;
         }
 
@@ -40,6 +43,8 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Put(Guid id, UpdatetemDto updateItemDto)
         {
             var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (existingItem == null) return NotFound();
 
             var updatedItem = existingItem with
             {
@@ -58,6 +63,9 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Delete(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0) return NotFound();
+
             items.RemoveAt(index);
 
             return NoContent();
