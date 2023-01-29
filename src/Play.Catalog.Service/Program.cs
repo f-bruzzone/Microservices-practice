9f31bfe1e1ab8ea7895765070c582b18efc87734
f-bruzzone <fb88@njit.edu>
2023-01-28 19:34:12 -0500

    simplified program.cs by breaking out some initialization code

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,14 +1,7 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
-using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
 // Add services to the container.
 
@@ -20,23 +13,7 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-    {
-        var database = serviceProvider.GetService<IMongoDatabase>();
-        return new MongoRepository<Item>(database, "items");
-    }
-);
-
-// Creates a more readable representation of the bson data in the mongo vs code etension
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+builder.Services.AddMongo().AddMongoRepository<Item>("items");
 
 var app = builder.Build();
 
